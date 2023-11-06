062664b2fc3394249407e792e7290bf07eba0627
JoHyunwoo <jhw3874@gmail.com>
2023-11-06 18:09:23 +0900

    git init

---
+++ keypang/src/main/java/portfolio/keypang/KeypangApplication.java
@@ -0,0 +1,13 @@
+package portfolio.keypang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeypangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeypangApplication.class, args);
+	}
+
+}
---
+++ keypang/src/test/java/portfolio/keypang/KeypangApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.keypang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeypangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
