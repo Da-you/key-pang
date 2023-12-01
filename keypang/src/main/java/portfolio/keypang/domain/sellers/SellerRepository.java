579cfac80dcfc397d9fb84139433f291b6550853
JoHyunwoo <jhw3874@gmail.com>
2023-12-01 14:51:37 +0900

    #13 : 판매자_도메인추가 - 인증된 사용자는 판매자로도 등록이 가능하다. (@OneToOne)사용

---
+++ keypang/src/main/java/portfolio/keypang/domain/sellers/SellerRepository.java
@@ -0,0 +1,7 @@
+package portfolio.keypang.domain.sellers;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerRepository extends JpaRepository<Seller,Long> {
+
+}
