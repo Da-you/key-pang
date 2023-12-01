d1f5de44cb84082f102ac2dc532447bdd4570054
JoHyunwoo <jhw3874@gmail.com>
2023-12-01 15:19:52 +0900

    #13 : 상품_도메인추가 - 판매자는 상품을 여러개의 상품을 가질수있다. (@ManyToOne)

---
+++ keypang/src/main/java/portfolio/keypang/domain/Item/common/WorkType.java
@@ -0,0 +1,10 @@
+package portfolio.keypang.domain.Item.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum WorkType {
+  청축, 적축, 갈축, 흑축
+}
