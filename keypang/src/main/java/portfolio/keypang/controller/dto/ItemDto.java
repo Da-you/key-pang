b0223df38dace85b4cad4f9e999204851ffd2769
JoHyunwoo <jhw3874@gmail.com>
2023-12-05 00:53:39 +0900

    #15 : 판매자, 상품 등록 구현 - 상품 등록에 s3 적용

--- keypang/src/main/java/portfolio/keypang/controller/dto/ItemDto.java
+++ keypang/src/main/java/portfolio/keypang/controller/dto/ItemDto.java
@@ -17,7 +17,6 @@ public class ItemDto {
 
     private String name;
     private Integer price;
-    private String thumbNail;
     private KeyboardType keyboardType;
     private WireType wireType;
     private WorkType workType;
